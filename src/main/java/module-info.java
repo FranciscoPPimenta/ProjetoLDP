d97d07f65c6e7b88d762ea03840873201241f9bd
FranciscoPPimenta <francisco.p.pimenta@outlook.com>
2025-03-14 12:39:55 +0000

    Começo do Trabalho

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.projetoldp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.projetoldp to javafx.fxml;
+    exports org.example.projetoldp;
+}
